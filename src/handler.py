85a73405b4aa2d727d67afc1c5bcf6c0185501b6
James Wu <therealjameswu@gmail.com>
2019-04-25 21:51:45 -0400

    Add insertion functionality

--- src/handler.py
+++ src/handler.py
@@ -1,16 +1,22 @@
 import json
-import beautifulsoup4
+import requests
+from utilities.sitescraper import retrieve_articles
+from utilities.constants import API_ENDPOINT_LOAD, TIMEOUT_SECONDS
 
 
 def load_data(event, context):
     body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
+        'message': 'Go Serverless v1.0! Your function executed successfully!',
+        'input': event
     }
 
+    articles = retrieve_articles()
+
+    insert(articles)
+
     response = {
-        "statusCode": 200,
-        "body": json.dumps(body)
+        'statusCode': 200,
+        'body': json.dumps(body)
     }
 
     return response
@@ -23,3 +29,26 @@ def load_data(event, context):
         "event": event
     }
     """
+
+
+def insert(articles):
+    for article in articles:
+        payload = {
+            'image_url': article['image_url'],
+            'title': article['title'],
+            'title_link': article['title_link'],
+            'category': article['category'],
+            'meta_content': article['meta_content']
+        }
+
+        response = requests.post(
+            API_ENDPOINT_LOAD, data=payload, timeout=TIMEOUT_SECONDS)
+
+        if '20' in str(response.status_code):
+            print('SUCCESSFULLY LOADED {}'.format(payload))
+        else:
+            print('FAILED TO LOAD {}'.format(payload))
+
+
+if __name__ == '__main__':
+    load_data(None, None)
